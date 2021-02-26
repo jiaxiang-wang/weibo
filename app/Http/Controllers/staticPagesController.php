eed3f36e355f8ad4a3d790ac61fccf266783c838
王佳祥 <18834233639@163.com>
2021-02-26 14:22:35 +0000

    22:22

--- app/Http/Controllers/staticPagesController.php
+++ app/Http/Controllers/staticPagesController.php
@@ -10,16 +10,16 @@ class staticPagesController extends Controller
     //主页
     public function home(){
         // return '主页';
-        return view('static_pages/home');
+        return view('/static_pages/home');
     }
     //帮助页
     public function help(){
         //return '帮助页';
-        return view('static_pages/help');
+        return view('/static_pages/help');
     }
     //关于页
     public function about(){
         //return '关于页';
-        return view('static_pages/about');
+        return view('/static_pages/about');
     }
 }
