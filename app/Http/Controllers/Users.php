a6e337257a9307c3ca60022deb8531698eb7b596
王佳祥 <18834233639@163.com>
2021-02-27 13:59:25 +0000

    完成用户注册功能

--- app/Http/Controllers/Users.php
+++ app/Http/Controllers/Users.php
@@ -23,15 +23,32 @@ class Users extends Controller
         return view('users.show',compact('user'));
     }
     /*
-        注册用户提交到store方法
+    册用户提交到store方法
+    User::create() 创建成功后会返回一个用户对象
+    route() 方法会自动获取 Model 的主键
+    函数 redirect() 可以将用户重定向到不同的页面
+
+    session() 方法来访问会话实例
+    只存入一条缓存的数据，让它只在下一次的请求内有效时，则可以使用 flash 方法
+    flash 方法接收两个参数，第一个为会话的键，第二个为会话的值
     */
     public function store(Request $request)
     {
+        //验证表单
         $this->validate($request, [
             'name' => 'required|unique:users|max:50',
             'email' => 'required|email|unique:users|max:255',
             'password' => 'required|confirmed|min:6'
         ]);
-        return;
+        //验证成功后把数据保存到数据库
+        //
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+        //存入一条临时数据 只在下一次请求前有效
+        session()->flash('success','欢迎，您将在这里开启一段新的旅程~0.0');
+        return redirect()->route('users.show',[$user]);
     }
 }
