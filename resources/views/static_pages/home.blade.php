d5ef7517da37fda36c7db17ec33fb914528152bb
王佳祥 <18834233639@163.com>
2021-03-02 08:00:38 +0000

    发布微博

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,15 +1,30 @@
 @extends('layouts.default')
 @section('title','首页')
 @section('content')
-<div class="jumbotron">
-  <h1>你好！</h1>
-  <p class="lead">
-   欢迎访问我的博客!
-  </p>
-  <p>一切从这里开始</p>
-  <p>
-    <a href="{{route('signup')}}" class="btn btn-lg btn-success" role="button">现在注册</a>
-  </p>
-</div>
+  @if(Auth::check())
+  <div class="row">
+      <div class="col-md-8">
+        <section class="status_form">
+          @include('shared._status_form')
+        </section>
+      </div>
+      <aside class="col-md-4">
+        <section class="user_info">
+          @include('shared._user', ['user' => Auth::user()])
+        </section>
+      </aside>
+    </div>
+  @else
+    <div class="jumbotron">
+      <h1>你好！</h1>
+      <p class="lead">
+      欢迎访问我的博客!
+      </p>
+      <p>一切从这里开始</p>
+      <p>
+        <a href="{{route('signup')}}" class="btn btn-lg btn-success" role="button">现在注册</a>
+      </p>
+    </div>
+  @endif
 @stop
 
